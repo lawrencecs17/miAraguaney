450606a3fa48d9981f8e66d5c1d5378e9382762d
Sara Villarreal <Sara Villarreal@SaraVillarreal>
2012-05-25 01:42:56 -0430

    Sara probando

---
+++ miAraguaney/grails-app/domain/miaraguaney/Sara.groovy
@@ -0,0 +1,7 @@
+package miaraguaney
+
+class Sara {
+
+    static constraints = {
+    }
+}
---
+++ miAraguaney/test/unit/miaraguaney/SaraTests.groovy
@@ -0,0 +1,17 @@
+package miaraguaney
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Sara)
+class SaraTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
