c9f2ac5f90959395c455d9d20c61f75aa7920a40
ricardo portela <ricky663@gmail.com>
2012-05-28 22:14:06 -0430

    subiendo la clase ComentarioCliente

---
+++ miAraguaney/grails-app/domain/miaraguaney/ComentarioCliente.groovy
@@ -0,0 +1,46 @@
+package miaraguaney
+
+import java.util.List;
+import java.util.Date;
+import miaraguaney.Comentario;
+import miaraguaney.Etiqueta;
+import miaraguaney.Usuario;
+
+/**
+*
+* @author Ricardo Portela
+* Clase destinada a gestionar todas las funcionalidades de ComentarioCliente 
+* con respecto al servidor
+*
+*/
+class ComentarioCliente {
+
+	String mensaje
+	String fecha
+	String autor
+	String cantidadLike
+	String cantidadDislike
+	String cantidadComentados
+	boolean principal
+	
+	List<String> adjuntos = new ArrayList<String>()
+	List<String> tag = new ArrayList<String>()
+	List<ComentarioCliente> comentado = new ArrayList<ComentarioCliente>()
+		
+	/**
+	* Restricciones básicas de la clase
+	*/
+	
+    static constraints = {
+		mensaje(blank:false)
+    }
+	
+	static mapping = {
+		sort fecha:   "asc"
+	}
+	/**
+	* Persistencia almacenada en una base de datos MongoDB
+	*/
+   
+   static mapWith = "mongo"
+}
