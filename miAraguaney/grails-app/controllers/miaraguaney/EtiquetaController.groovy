e1094d9f0b0a4b469fd0ee4d2856ae7956d27744
ricardo portela <ricky663@gmail.com>
2012-06-13 15:32:23 -0430

    creacion del try catch de EtiquetaController

--- miAraguaney/grails-app/controllers/miaraguaney/EtiquetaController.groovy
+++ miAraguaney/grails-app/controllers/miaraguaney/EtiquetaController.groovy
@@ -11,64 +11,72 @@ class EtiquetaController {
 
 	private static Log log = LogFactory.getLog("Logs."+UsuarioController.class.getName())
 	
-    def index() { 
-		
-		log.info("Ejemplo de este log")
-		render (view:'consultarTodos')
-	}
+	    def index() { 
+			
+			log.info("Ejemplo de este log")
+			render (view:'consultarTodos')
+		}
 	
-	/**
-	* Invocacion al servicio de consultar todas las Etiquetas
-	* registrados en el sistema
-	*/
-   def consultarTodasLasEtiquetas = {
-	   
-	   /**
-		* Se ubica la URL del servicio que lista a todas las Etiquetas
+		/**
+		* Invocacion al servicio de consultar todas las Etiquetas
+		* registrados en el sistema
 		*/
-	   def url = new URL("http://localhost:8080/miOrquidea/etiqueta/listarTodos" )
-	   def listaEtiqueta
-	   
-	   /**
-		* Se establece la conexion con el servicio
-		* Se determina el tipo de peticion (GET) y
-		* el contenido de la misma (Archivo plano XML)
-		*/
-	   def connection = url.openConnection()
-	   connection.setRequestMethod("GET" )
-	   connection.setRequestProperty("Content-Type" ,"text/xml" )
-	   
-	   if(connection.responseCode == 200)
+	   def consultarTodasLasEtiquetas = {
+		   
+	   try
 	   {
-		   def miXml = new XmlSlurper().parseText(connection.content.text)
-		   listaEtiqueta = procesarXmlEtiqueta(miXml)
+		   /**
+			* Se ubica la URL del servicio que lista a todas las Etiquetas
+			*/
+		   def url = new URL("http://localhost:8080/miOrquidea/etiqueta/listarTodos" )
+		   def listaEtiqueta
+		   
+		   /**
+			* Se establece la conexion con el servicio
+			* Se determina el tipo de peticion (GET) y
+			* el contenido de la misma (Archivo plano XML)
+			*/
+		   def connection = url.openConnection()
+		   connection.setRequestMethod("GET" )
+		   connection.setRequestProperty("Content-Type" ,"text/xml" )
+		   
+		   if(connection.responseCode == 200)
+		   {
+			   def miXml = new XmlSlurper().parseText(connection.content.text)
+			   listaEtiqueta = procesarXmlEtiqueta(miXml)
+		   }
+		   else{
+			   render "Se ha generado un error:"
+			   render connection.responseCode
+			   render connection.responseMessage
+		   }
+		   
+		   render (view:"consultarEtiquetas", model:[etiquetas:listaEtiqueta, usuario:session.nickname])   
 	   }
-	   else{
-		   render "Se ha generado un error:"
-		   render connection.responseCode
-		   render connection.responseMessage
+	   catch(Exception)
+	   {
+		   def miAlerta = "Ha ocurrido un error en el servidor, intente luego."
+		   render(view:"errorEtiqueta",model:[email:session.email,usuario:session.nickname,alerta:miAlerta])
 	   }
+	   }// fin consultarTodasLasEtiquetas
 	   
-	   render (view:"consultarEtiquetas", model:[etiquetas:listaEtiqueta, usuario:session.nickname])   
-   }
-   
-   /**
-   * Metodo encargado de procesar el archivo XML recibido del
-   * servicio miOrquidea app
-   * @param xml
-   * @return
-   */
-  def procesarXmlEtiqueta(def xml)
-  {
-	  ArrayList<Etiqueta> listaEtiqueta = new ArrayList<Etiqueta>()
-	  
-	  for (int i=0;i< xml.etiqueta.size();i++)
+	   /**
+	   * Metodo encargado de procesar el archivo XML recibido del
+	   * servicio miOrquidea app
+	   * @param xml
+	   * @return
+	   */
+	  def procesarXmlEtiqueta(def xml)
 	  {
-		  Etiqueta etiqueta = new Etiqueta()
-		  etiqueta.nombre = xml.etiqueta[i].nombre
-		  listaEtiqueta.add(etiqueta.nombre)
+		  ArrayList<Etiqueta> listaEtiqueta = new ArrayList<Etiqueta>()
+		  
+		  for (int i=0;i< xml.etiqueta.size();i++)
+		  {
+			  Etiqueta etiqueta = new Etiqueta()
+			  etiqueta.nombre = xml.etiqueta[i].nombre
+			  listaEtiqueta.add(etiqueta.nombre)
+		  }
+		  return listaEtiqueta
 	  }
-	  return listaEtiqueta
-  }
   
 }
